ee68a1ae98b9221c21b8fc72ed07c6236b7996c3
imperfectSD <imperfect.sparda@gmail.com>
2021-10-18 13:31:46 +0300

    Junit4 +1 failed test

--- src/test/java/ru/netology/service/CashbackHackSertviceTest.java
+++ src/test/java/ru/netology/service/CashbackHackSertviceTest.java
@@ -16,6 +16,17 @@ public class CashbackHackSertviceTest {
     }
 
     @org.junit.Test
+    public void shouldRemain0IfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
     public void shouldRemain500IfAmountIs1500() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1500;
