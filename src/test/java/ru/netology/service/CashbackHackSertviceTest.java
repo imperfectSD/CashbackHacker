62b57143f0991d574ae7d5fbbe73e27e589e9824
imperfectSD <imperfect.sparda@gmail.com>
2021-10-18 13:29:24 +0300

    TestNG +1 failed test

--- src/test/java/ru/netology/service/CashbackHackSertviceTest.java
+++ src/test/java/ru/netology/service/CashbackHackSertviceTest.java
@@ -16,6 +16,17 @@ public class CashbackHackSertviceTest {
     }
 
     @org.testng.annotations.Test
+    public void shouldRemain0IfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
     public void shouldRemain500IfAmountIs1500() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1500;
